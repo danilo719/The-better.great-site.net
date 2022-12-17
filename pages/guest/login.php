6012b7644378429e9fc88a2735d8407136ac4d36
danilo719 <d.roumans719@gmail.com>
2022-12-17 20:57:56 +0100

    login v1

--- pages/guest/login.php
+++ pages/guest/login.php
@@ -1,17 +1,28 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        if(isset($_POST['mail']) && isset($_POST['Wachtwoord']))
+        if(isset($_POST['username']) && isset($_POST['password']))
         {
-            $var = fgetobject('gebruikers', 'mail', strtolower($_POST['mail']));
+            $var = fgetobject('users', 'username', strtolower($_POST['username']));
 
             $selected = $var->fetch(PDO::FETCH_ASSOC);
 
-            if(password_verify($_POST['Wachtwoord'], $selected['wachtwoord']) && $selected['mail'] == strtolower($_POST['mail']))
+            $today = date("Y-m-d");
+
+            if($selected['enddate'] <= $today){
+
+              $_SESSION["alert"]["backgroundcolor"] = "red";
+              $_SESSION["alert"]["color"] = "white";
+              $_SESSION["alert"]["message"] = "oeps ".$selected['username'].", uw account is verlopen";
+              header('Location: index.php?path=guest&page=login');
+              die();
+            }
+
+            if(password_verify($_POST['password'], $selected['password']) && $selected['username'] == strtolower($_POST['username']))
             {
                 $_SESSION["ingelogd"] = true;
-                $_SESSION["gebruikersID"] = $selected["gebruikersID"];
-                $_SESSION["rechten_ID"] = $selected["rechten_ID"];
+                $_SESSION["userID"] = $selected["userID"];
+                $_SESSION["roleID"] = $selected["roleID"];
 
                 $_SESSION["alert"]["backgroundcolor"] = "green";
                 $_SESSION["alert"]["color"] = "white";
@@ -23,7 +34,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             {
                 $_SESSION["alert"]["backgroundcolor"] = "red";
                 $_SESSION["alert"]["color"] = "white";
-                $_SESSION["alert"]["message"] = "oeps, u heeft niet de goede combinatie voor eem mail en wachtwoord";
+                $_SESSION["alert"]["message"] = "oeps, u heeft niet de goede combinatie voor eem gebruikersnaam en wachtwoord";
                 header('Location: index.php?path=guest&page=login');
                 die();
             }
@@ -38,62 +49,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         }
     }
 ?>
-<style>
-.gradient-custom {
-/* fallback for old browsers */
-background: #6a11cb;
+<style>body{
+  margin: 0;
+  padding: 0;
+  background: url(https://i.ibb.co/VQmtgjh/6845078.png) no-repeat;
+  height: 100vh;
+  font-family: sans-serif;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  overflow: hidden}@media screen and (max-width: 600px;)
+  {body{background-size: cover;: fixed}}#particles-js{height: 100%}.loginBox{position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);width: 350px;min-height: 200px;background: #000000;border-radius: 10px;padding: 40px;box-sizing: border-box}.user{margin: 0 auto;display: block;margin-bottom: 20px}h3{margin: 0;padding: 0 0 20px;color: #59238F;text-align: center}.loginBox input{width: 100%;margin-bottom: 20px}.loginBox input[type="text"], .loginBox input[type="password"]{border: none;border-bottom: 2px solid #262626;outline: none;height: 40px;color: #fff;background: transparent;font-size: 16px;padding-left: 20px;box-sizing: border-box}.loginBox input[type="text"]:hover, .loginBox input[type="password"]:hover{color: #42F3FA;border: 1px solid #42F3FA;box-shadow: 0 0 5px rgba(0,255,0,.3), 0 0 10px rgba(0,255,0,.2), 0 0 15px rgba(0,255,0,.1), 0 2px 0 black}.loginBox input[type="text"]:focus, .loginBox input[type="password"]:focus{border-bottom: 2px solid #42F3FA}.inputBox{position: relative}.inputBox span{position: absolute;top: 10px;color: #262626}.loginBox input[type="submit"]{border: none;outline: none;height: 40px;font-size: 16px;background: #59238F;color: #fff;border-radius: 20px;cursor: pointer}.loginBox a{color: #262626;font-size: 14px;font-weight: bold;text-decoration: none;text-align: center;display: block}a:hover{color: #00ffff}p{color: #0000ff}</style>
 
-/* Chrome 10-25, Safari 5.1-6 */
-background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
-
-/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
-
-height: 100%;
-} 
-</style>
-<section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style="border-radius: 1rem;">
-          <div class="card-body p-5 text-center">
-
-            <div class="mb-md-5 mt-md-4 pb-5">
-
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
-
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">Email</label>
-              </div>
-
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                <label class="form-label" for="typePasswordX">Password</label>
-              </div>
-
-              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-
-              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-              <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-              </div>
-
-            </div>
-
-            <div>
-              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
-              </p>
-            </div>
-
-          </div>
-        </div>
-      </div>
+<div class="loginBox"> <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px">
+        <h3>Login</h3>
+        <form action="index.php?path=guest&page=login" method="post">
+            <div class="inputBox"> 
+              <input type="text" name="username" placeholder="Gebruikersnaam"> 
+              <input type="password" name="password" placeholder="Wachtwoord"> 
+            </div> 
+            <input type="submit" name="" value="Inloggen">
+        </form>         
     </div>
-  </div>
-</section>
