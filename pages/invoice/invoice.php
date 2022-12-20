0991cdf0888ee59b32b45609759887704ae0d281
Nacedo Zorgvol <120800802+NacedoZorgvol@users.noreply.github.com>
2022-12-20 22:45:24 +0100

    "CSS Linkjes toegevoegd aan de invoice pagina(layout)  en git pull van main naar invoice-branche"

--- pages/invoice/invoice.php
+++ pages/invoice/invoice.php
@@ -1,3 +1,25 @@
+<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"
+  rel="stylesheet"
+/>
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"
+></script>
+
 <div class="card">
 <div class="card-body mx-4">
   <div class="container">
